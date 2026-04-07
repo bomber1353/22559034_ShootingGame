74c36df495fe906b42b60946cdf38b98cdc7e1c9
bomber1353 <khsuper282@gmail.com>
2026-04-07 19:54:43 +0900

    5주차 과제 최종제출

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -14,6 +14,6 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.up * spd * Time.deltaTime);
+        transform.Translate(Vector3.forward * spd * Time.deltaTime);
     }
 }
